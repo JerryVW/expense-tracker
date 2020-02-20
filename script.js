62900b82a65c90baa070ccbea028255f5fb5b0b7
JerryVW <jerryvw72@gmail.com>
2020-02-20 12:54:57 -0500

    Got data to show up in rows properly and delete button to work

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 window.onload = function() {
-    const typeArray = JSON.parse(localStorage.getItem("tab_data1")) || [];
-    const purchaseArray = JSON.parse(localStorage.getItem("tab_data2")) || [];
-    const dateArray = JSON.parse(localStorage.getItem("tab_data3")) || [];
-    const amountArray = JSON.parse(localStorage.getItem("tab_data4")) || [];
     document.getElementById("add-button").addEventListener("click", createDataRow);
-    typeArray.forEach(function(item) {
-        createDataRow(item);
-    });
-
-    purchaseArray.forEach(function(item) {
-        createDataRow(item);
-    });
-
-    dateArray.forEach(function(item) {
-        createDataRow(item);
-    });
-
-    amountArray.forEach(function(item) {
-        createDataRow(item);
-    });
 }
 
 let type;
@@ -26,16 +7,31 @@ let purchase;
 let date;
 let amount;
 
+// const typeArray = JSON.parse(localStorage.getItem("tab_data1")) || [];
+// const purchaseArray = JSON.parse(localStorage.getItem("tab_data2")) || [];
+// const dateArray = JSON.parse(localStorage.getItem("tab_data3")) || [];
+// const amountArray = JSON.parse(localStorage.getItem("tab_data4")) || [];
+
 function createDataRow(e) {
     e.preventDefault();
-        let expenseRow = document.getElementById("empty-row");
-        const tr = document.createElement("tr");
+    let tableBody = document.querySelector("tbody");
+    const tr = document.createElement("tr");
+    tableBody.appendChild(tr);
 
-        tr.appendChild(paymentType());
-        tr.appendChild(purchaseType());
-        tr.appendChild(dateOfPurchase());
-        tr.appendChild(amountOfPurchase());
-        expenseRow.appendChild(tr);
+    document.createElement("td");
+    tr.appendChild(paymentType());
+    
+    document.createElement("td");
+    tr.appendChild(purchaseType());
+
+    document.createElement("td");
+    tr.appendChild(dateOfPurchase());
+
+    document.createElement("td");
+    tr.appendChild(amountOfPurchase());
+
+    document.createElement("td");
+    tr.appendChild(createDeleteButton());
 }
 
 function paymentType() {
@@ -88,13 +84,28 @@ function amountOfPurchase() {
     localStorage.setItem("tab_data4", JSON.stringify(amountArray));
     amount = document.getElementById("amount").value = "";
     console.log(amountArray);
+    createDeleteButton();
 
     return td4;
 }
 
+function createDeleteButton() {
+    const button = document.createElement("button");
+    button.className = "to-delete";
+    button.textContent = "X";
+    button.addEventListener("click", function(e) {
+        if(e.target.classList.contains("to-delete")){
+            let expenseListItem = document.querySelector("tbody");
+            const tr = e.target.parentElement;
+            expenseListItem.removeChild(tr);
+            const listArray = JSON.parse(localStorage.getItem("tab_data1", "tab_data2", "tab_data3", "tab_data4")) || [];
+            let arrayList = listArray.indexOf(e.target.textContent);
+            for (key of listArray) {
+                listArray.splice(arrayList, 1);
+            }
+            localStorage.setItem("tab_data1", "tab_data2", "tab_data3", "tab_data4", JSON.stringify(listArray));
+        }
+    });
 
-
-const listArray = JSON.parse(localStorage.getItem("list-item")) || [];
-    listArray.push(newItem);
-    localStorage.setItem("list-item", JSON.stringify(listArray));
-    newItem = document.getElementById("item").value = "";
+    return button;
+}
